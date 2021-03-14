e20857d80055a2e3d13886af1e5c9815a3804dd2
JorgeSerranoP <100405987@alumnos.uc3m.es>
2021-03-14 19:12:43 +0100

    Creados componentes comunidad y provincia

--- Micropractica3/src/app/app.module.ts
+++ Micropractica3/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComunidadComponent } from './comunidad/comunidad.component';
+import { ProvinciaComponent } from './provincia/provincia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComunidadComponent,
+    ProvinciaComponent
   ],
   imports: [
     BrowserModule,
